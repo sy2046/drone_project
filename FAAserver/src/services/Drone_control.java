cd4da22896a0e15b4e6449dfc1ed40f1ed55e9ab
Ye Sun <sun.ye@etu.unice.fr>
2016-01-30 13:49:37 +0100

    GET method

--- FAAserver/src/services/Drone_control.java
+++ FAAserver/src/services/Drone_control.java
@@ -10,6 +10,7 @@ public class Drone_control {
 
 	// This method is called if TEXT_PLAIN is request
 	@GET
+	@Path("/welcome")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String welcome() {
 		return "Welcome to FAA Web Service";
@@ -17,6 +18,7 @@ public class Drone_control {
 
 	// This method is called if XML is request
 	@GET
+	@Path("/welcome.xml")
 	@Produces(MediaType.TEXT_XML)
 	public String welcomeXML() {
 		return "<?xml version=\"1.0\"?>" + "<title> Welcome to FAA Web Service" + "</title>";
@@ -24,6 +26,7 @@ public class Drone_control {
 
 	// This method is called if HTML is request
 	@GET
+	@Path("/welcome.html")
 	@Produces(MediaType.TEXT_HTML)
 	public String welcomeHTML() {
 		return "<html> " + "<title>" + "Welcome" + "</title>" + "<body><h1>" + "Welcome to FAA Web Service" + "</body></h1>"
