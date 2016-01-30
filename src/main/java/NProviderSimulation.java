a3f0f1372b36b0629ec012237e469e70fd89e70b
Youssef Salim <salimyoussef11@yahoo.fr>
2016-01-30 21:47:16 +0100

    Simulation avec GooglePathFinder et 50 drones

--- src/main/java/NProviderSimulation.java
+++ src/main/java/NProviderSimulation.java
@@ -1,4 +1,8 @@
+import endPoints.AdressEndPoints;
+import path.Path;
 import path.PathPoint;
+import pathFinder.GooglePathFinder;
+import pathFinder.PathPlannerStrategy;
 import pathToNavCommands.CommandsProvider;
 import utils.MyConstants;
 
@@ -28,37 +32,30 @@ public class NProviderSimulation {
         public void run() {
             Random genetator = new Random();
             //String drone = EventMediatorLocator.mediator().allocateDrone();
-               /* String dest[] = {"Dijon", "Lille", "Rennes", "Tours", "Caen", "Nantes", "Angers"};
-                Random random = new Random();
-                int idx1 = random.nextInt(7);
-                //String destination = dest[idx1];
-                String destination = "Paris";
-                int idx2 = random.nextInt(7);
+            //String dest[] = {"Dijon", "Lille", "Rennes", "Tours", "Caen", "Nantes", "Angers"};
+            // Random random = new Random();
+            //int idx1 = random.nextInt(7);
+            //String destination = dest[idx1];
+            String destination = "Paris";
+               /* int idx2 = random.nextInt(7);
                 if (idx2 == idx1) {
                     idx2 = (idx2 + 1) % dest.length;
                 }*/
 
-                /*ArrayList<PathPoint> path = new ArrayList<>();
-                for(int i=0; i<20;i++){
-                    path.add(new PathPoint(10+i,20,12));
-                }*/
-
             //String start = dest[idx2];
+            String start = "Lille";
+            PathPlannerStrategy pathPlanner = new GooglePathFinder();
+            System.out.println(start + ":" + destination);
+            Path p = pathPlanner.findPath(new AdressEndPoints(start, destination));
 
-            ArrayList<PathPoint> path = new ArrayList<>();
+            /*ArrayList<PathPoint> path = new ArrayList<>();
             for(int i=0; i<30;i++){
                 path.add(new PathPoint(10+i,20,12));
-            }
+            }*/
 
-            String start = "Meaux";
-            //PathPlannerStrategy pathPlanner = new GooglePathFinder();
-            //System.out.println(start + ":" + destination);
-            //Path p = pathPlanner.findPath(new AdressEndPoints(start, destination));
-            // EventMediatorLocator.mediator().loadPath(drone, p.getPathPoints());
             CommandsProvider provider = new CommandsProvider("drone"+id);
-            provider.setPath(path);
+            provider.setPath(p.getPathPoints());
             provider.sendCommands();
-            //EventMediatorLocator.mediator().startDelivary(drone);
         }
 
     }
--- src/main/java/NTracerSimulation.java
+++ src/main/java/NTracerSimulation.java
@@ -1,3 +1,7 @@
+import endPoints.AdressEndPoints;
+import path.Path;
+import pathFinder.GooglePathFinder;
+import pathFinder.PathPlannerStrategy;
 import tracer.Tracer;
 import utils.MyConstants;
 
@@ -22,23 +26,6 @@ public class NTracerSimulation {
 
         @Override
         public void run() {
-            Random genetator = new Random();
-            //String drone = EventMediatorLocator.mediator().allocateDrone();
-               /* String dest[] = {"Dijon", "Lille", "Rennes", "Tours", "Caen", "Nantes", "Angers"};
-                Random random = new Random();
-                int idx1 = random.nextInt(7);
-                //String destination = dest[idx1];
-                String destination = "Paris";
-                int idx2 = random.nextInt(7);
-                if (idx2 == idx1) {
-                    idx2 = (idx2 + 1) % dest.length;
-                }*/
-
-                //String start = dest[idx2];
-                String start = "Meaux";
-                //PathPlannerStrategy pathPlanner = new GooglePathFinder();
-                //System.out.println(start + ":" + destination);
-                //Path p = pathPlanner.findPath(new AdressEndPoints(start, destination));
                 //int nb_topics_per_zookeeper = MyConstants.NUMBER_OF_DRONES/MyConstants.NUMBER_OF_ZOOKEEPER;
                 Tracer tracer = new Tracer("tracer" + id, "drone" + id, "localhost:" + MyConstants.KAFKA_ZK_PORT); //+ (id/nb_topics_per_zookeeper)));
                 tracer.run(1);
