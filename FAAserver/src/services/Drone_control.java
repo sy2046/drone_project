656e1bbd4bf3af56e54d93ad93ce453c65e50af2
Ye Sun <sun.ye@etu.unice.fr>
2016-01-30 13:57:25 +0100

    POST Method

--- FAAserver/src/services/Drone_control.java
+++ FAAserver/src/services/Drone_control.java
@@ -1,9 +1,17 @@
 package services;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+ 
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/drone")
 public class Drone_control {
@@ -29,7 +37,27 @@ public class Drone_control {
 	@Path("/welcome.html")
 	@Produces(MediaType.TEXT_HTML)
 	public String welcomeHTML() {
-		return "<html> " + "<title>" + "Welcome" + "</title>" + "<body><h1>" + "Welcome to FAA Web Service" + "</body></h1>"
-				+ "</html> ";
+		return "<html> " + "<title>" + "Welcome" + "</title>" + "<body><h1>" + "Welcome to FAA Web Service"
+				+ "</body></h1>" + "</html> ";
+	}
+
+	@POST
+	@Path("/path")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response pathREST(InputStream incomingData) {
+		StringBuilder pathBuilder = new StringBuilder();
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+			String line = null;
+			while ((line = in.readLine()) != null) {
+				pathBuilder.append(line);
+			}
+		} catch (Exception e) {
+			System.out.println("Error Parsing: - ");
+		}
+		System.out.println("Path Received: " + pathBuilder.toString());
+
+		// return HTTP response 200 in case of success
+		return Response.status(200).entity(pathBuilder.toString()).build();
 	}
 }
